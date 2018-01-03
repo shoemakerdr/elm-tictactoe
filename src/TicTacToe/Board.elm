def761f3e1766e13c45070e65c1ba685ffa4dc14
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-03 12:23:37 -0600

    Added implementation for addMove and isSpaceOccupied

--- src/TicTacToe/Board.elm
+++ src/TicTacToe/Board.elm
@@ -3,17 +3,25 @@ module TicTacToe.Board exposing
   , Space(..)
   , Player(..)
   , reset
+  , addMove
+  , isSpaceOccupied
   )
 
+import Array
+
+
 type alias Board = List Space
+
 
 type Player
   = X
   | O
 
+
 type Space
   = Empty
   | Occupied Player
+
 
 reset : Board -> Board
 reset board =
@@ -22,3 +30,26 @@ reset board =
   , Empty, Empty, Empty
   ]
 
+
+addMove : Player -> Int -> Board -> Board
+addMove player index board =
+  Array.fromList board
+    |> Array.set index (Occupied player)
+    |> Array.toList
+
+
+isSpaceOccupied : Int -> Board -> Bool
+isSpaceOccupied index board =
+  let
+    arrBoard = Array.fromList board
+    space = Array.get index arrBoard
+  in
+    case space of
+      Nothing ->
+        True
+
+      Just Empty ->
+        False
+
+      Just (Occupied a) ->
+        True
