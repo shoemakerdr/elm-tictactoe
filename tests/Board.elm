49bc908dcf4a66666d3e9d4906a5b98efa9c75ea
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-03 12:23:20 -0600

    Added tests for addMove and isOccupied

--- tests/Board.elm
+++ tests/Board.elm
@@ -8,12 +8,14 @@ import TicTacToe.Board exposing
   , Space(..)
   , Player(..)
   , reset
+  , addMove
+  , isSpaceOccupied
   )
 
 
 suite : Test
 suite =
-  describe "Board functions"
+  describe "Board helper functions"
     [ describe "reset"
       [ test "should take an empty board and return an empty board" <|
           \_ ->
@@ -35,6 +37,33 @@ suite =
                 ]
             in
               Expect.equal emptyBoard (reset board)
+      ]
+    , describe "addMove"
+      [ test "should take a Player, an index, and a board and return a board with that Player occupying the board at that index" <|
+        \_ ->
+          let
+            board =
+              [ Occupied X, Empty, Empty
+              , Empty, Empty, Empty
+              , Empty, Empty, Empty
+              ]
+          in
+            Expect.equal board (addMove X 0 emptyBoard)
+      ]
+    , describe "isSpaceOccupied"
+      [ test "should return False when space is empty" <|
+          \_ ->
+            Expect.equal False (isSpaceOccupied 0 emptyBoard)
+      , test "should return True when space is occupied" <|
+          \_ -> 
+            let 
+              board = 
+                [ Occupied X, Empty, Empty
+                , Empty, Empty, Empty
+                , Empty, Empty, Empty
+                ]
+            in
+              Expect.equal True (isSpaceOccupied 0 board)
       ]
     ]
 
